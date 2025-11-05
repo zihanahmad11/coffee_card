818cc56bc41a0e44a78b0a54321f0ba02ec0b9f2
Yusuf Ahmad Zihan <zihanahmed360@gmail.com>
2025-11-05 22:18:09 +0600

    Initial Flutter project setup

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Coffee ID'),
+        backgroundColor: Colors.brown[500],
+        centerTitle: true,
+      ),
+      body: const Text('Welcome to My Coffee ID!'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
